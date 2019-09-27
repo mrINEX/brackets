2cd009df5a4c23c0868d02f33589b41df3648c9c
mrinex <mrinex@mail.ru>
2019-09-27 21:50:29 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let config = '';
+
+  for(i = 0; i < bracketsConfig.length; i++)
+  {
+    for(j = 0; j < 2; j++)
+    {
+      if((j === 0)&&(/\W/g.test(bracketsConfig[i][j]))){config += '\\';}
+       config += bracketsConfig[i][j]; ///main
+       if((j === 0)&&(/\W/g.test(bracketsConfig[i][j]))){config += '\\';}
+      if (j === 1)
+        config += '|';
+    }
+    if(i === bracketsConfig.length-1)
+    	config = config.replace(/\|$/,'');
+  }
+  let regex = RegExp(config,'g');
+  //-------------------------------------------
+  let del = str;
+  let boool = regex.test(del); ///last change
+  regex.lastIndex = 0; /// new
+    for (let i = 0; i < str.length/2; i++)
+    {
+      del = del.replace(regex,'');
+      if(del.length === 1)
+        return regex.test(del);
+    }
+    if(del === '')
+      return boool;
+  boool = regex.test(del);
+  return boool;
 }
